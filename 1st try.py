e2a62c5f48cfb4f04bf5c2f373f24d7ba8bc158a
rmarks11 <rachelmarks@knights.ucf.edu>
2021-08-30 19:50:00 -0400

    week 2 work in class

---
+++ 1st try.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print("Hello, World!\nHow do you do?")
+
+
+# In[6]:
+
+
+print("Hello, World!\nWhat should I do?\nWhere should I go?")
+
+
+# In[10]:
+
+
+print("2 + 3 is", 2 + 3)
+print("3 * 5 is", 3 * 5)
+print("100 - 2 is", 100 - 2)
+print("(33 + 3) / 5 + 11.5 is", (33 + 3) / 5 + 11.5)
+
+
+# In[ ]: 
+
+age = 20
+first_name = "John"
+last_name = "Smith"
+is_new = True
+print(first_name + " " + last_name) 
+
+
+
+
+
+# %%
+name = input("What is your name? ")
+print("Hello" + name)
+# %%
---
+++ python_for_beginners.py
@@ -0,0 +1,60 @@
+#name = input("What is your name? ")
+#print("Hello " + name)
+
+#birth_year = input("Enter your birth year: ")
+#age = 2021 - int(birth_year)
+#print(age)
+
+# first = input("Enter your first number: ")
+# second = input("Enter your second number: ")
+# sum = float(first) + float(second)
+# print("Sum: " + str(sum))
+
+# temperature = 9 
+
+# if temperature > 30: 
+#     print ("It's a hot day")
+#     print ("Drink plenty of water")
+# elif temperature > 20: #  (20, 30]
+#     print("It's a nice day")
+# elif temperature > 10: 
+#     print("It's a bit cold")
+# else: 
+#     print("It's cold")
+# print("done")
+
+# weight = input("What is your weight?: ")
+# unit = input("(K)g or (L)bs?: ")
+# if unit == "k" or unit == "K": 
+#     weight = float(weight)*2.2046
+#     print("Weight in (L)bs: " + str(weight))
+# if unit == "l" or unit == "L": 
+#     weight = float(weight)/2.2046
+#     print("Weight in (K)g: " + str(weight))
+
+# i = 1
+# while i <= 10: 
+#     print(i * "*")
+#     # i = i + 1
+
+# names = ["Philip", "Shannon", "Rachel", "Joshua", "Linda"]
+# names[0] = "Phillip"
+# # print(names[0:3])
+
+# numbers = [1,2,3,4,5]
+# print(len(numbers))
+
+# numbers = [1, 2, 3, 4, 5] 
+# for item in numbers: 
+#     print(item)
+
+# i = 0 
+# while i < len(numbers): 
+#     print(numbers[i])
+#     i = i + 1
+
+# for number in range(5): 
+#     print(number)
+
+# numbers = (1,2,3,3)
+# print(numbers.count(3))
